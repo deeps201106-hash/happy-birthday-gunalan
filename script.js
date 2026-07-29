de0c51481f3942fe139f20040b60886efee67ef8
deeps201106-hash <deeps201106@gmail.com>
2026-07-29 07:38:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -58,52 +58,7 @@ window.addEventListener("scroll", () => {
 function surprise() {
     alert("❤️ Happy Birthday My Love Guna ❤️\n\nI Love You Forever ❤️");
 }
-const canvas = document.getElementById("fireworks");
-const ctx = canvas.getContext("2d");
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-let particles = [];
-
-function createFirework() {
-    let x = Math.random() * canvas.width;
-    let y = Math.random() * canvas.height / 2;
-
-    for (let i = 0; i < 40; i++) {
-        particles.push({
-            x,
-            y,
-            dx: (Math.random() - 0.5) * 8,
-            dy: (Math.random() - 0.5) * 8,
-            life: 100
-        });
-    }
-}
-
-function animateFireworks() {
-    ctx.clearRect(0,0,canvas.width,canvas.height);
-
-    particles.forEach((p,index)=>{
-        p.x += p.dx;
-        p.y += p.dy;
-        p.life--;
-
-        ctx.beginPath();
-        ctx.arc(p.x,p.y,3,0,Math.PI*2);
-        ctx.fillStyle="gold";
-        ctx.fill();
-
-        if(p.life<=0){
-            particles.splice(index,1);
-        }
-    });
-
-    requestAnimationFrame(animateFireworks);
-}
-
-setInterval(createFirework,1500);
-animateFireworks();
 function openLetter(){
 document.getElementById("letterPopup").style.display="block";
 }
